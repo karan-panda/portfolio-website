4cbcfd9d1a47b6918be71cb0355e54be000fc532
mrpanda1206 <karanpanda1206@gmail.com>
2025-01-05 18:43:02 +0530

    Added Custom Cursor effect

---
+++ components/Cursor.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+
+const Cursor = () => {
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const circles = document.querySelectorAll(".circle");
+
+    circles.forEach(function (circle, index) {
+      circle.x = 0;
+      circle.y = 0;
+      circle.style.position = "absolute"; 
+    });
+
+    const handleMouseMove = (e) => {
+      setCoords({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    function animateCircles() {
+      let x = coords.x;
+      let y = coords.y;
+
+      circles.forEach(function (circle, index) {
+        circle.style.left = x - 5 + "px";
+        circle.style.top = y - 5 + "px";
+
+        circle.style.scale = (circles.length - index) / circles.length;
+
+        circle.x = x;
+        circle.y = y;
+
+        const nextCircle = circles[index + 1] || circles[0];
+        x += (nextCircle.x - x) * 0.2;
+        y += (nextCircle.y - y) * 0.2;
+      });
+
+      requestAnimationFrame(animateCircles);
+    }
+
+    animateCircles();
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [coords]);
+
+  return (
+    <>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+    </>
+  );
+};
+
+export default Cursor;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 // Home page
+import Cursor from "@/components/Cursor";
 
 export default function Home() {
   return (
     <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-        <h1 className="text-3xl font-bold text-center mt-10">Home</h1>
-        <p className="text-center mt-5">This is the home page.</p>
+      <Cursor />
+      <h1 className="text-3xl font-bold text-center mt-10">Home</h1>
+      <div className="bg-gray-800 p-6 mt-3 rounded-lg shadow-lg flex justify-center items-center">
+        <img src="https://readme-typing-svg.herokuapp.com/?font=Righteous&size=35&color=FFFFFF&center=true&vCenter=true&width=500&height=70&duration=4000&lines=Hi+There!+👋🏻;+I'm+Karan+Panda!⚡;" />
       </div>
+    </div>
   );
 }
